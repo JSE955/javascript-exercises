e3b1a98c5ea73e1dba6f33e974ee48c993b37493
JSE955 <jordanscottedwards@gmail.com>
2024-04-22 14:03:15 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || typeof a !== "number"
+     || typeof b !== "number") {
+        return "ERROR";
+    }
+    let result = 0;
+    if (b >= a) {
+        for (let i = a; i <= b; i++) {
+            result += i;
+        }
+    } else {
+        for (let i = b; i <= a; i++) {
+            result +=i;
+        }
+    }
+    return result;
 
 };
 
