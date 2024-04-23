6325484082d5334938edc63dd8483915ed9aac2a
JSE955 <jordanscottedwards@gmail.com>
2024-04-23 14:39:37 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const today = new Date();
+    return people.reduce((person, nextPerson) => {
+        let personAge = (person.yearOfDeath || today.getFullYear()) - person.yearOfBirth;
+        let nextPersonAge = (nextPerson.yearOfDeath || today.getFullYear()) - nextPerson.yearOfBirth;
+        if (personAge > nextPersonAge) {
+            return person;
+        } else {
+            return nextPerson;
+        }
+    })
 };
 
 // Do not edit below this line
