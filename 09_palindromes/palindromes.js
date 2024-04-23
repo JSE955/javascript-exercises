d1912d09908519e4fd1099297db398443c3bf935
JSE955 <jordanscottedwards@gmail.com>
2024-04-23 13:28:43 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let characterList = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let wordLowerCase = word.toLowerCase();
+    let wordAsArray = wordLowerCase.split("");
+    wordAsArray = wordAsArray.filter(character => characterList.includes(character));
+    let cleanWord = wordAsArray.join("");
+    let reversedWord = wordAsArray.reverse().join("");
+    if (cleanWord === reversedWord) {
+        return true;
+    }
+    return false;
+    
 };
 
 // Do not edit below this line
