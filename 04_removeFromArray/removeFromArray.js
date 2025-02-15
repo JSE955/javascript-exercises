76d5e91a6f7a57b69e1506aaf98c74d6d11b8ff8
JSE955 <jordanscottedwards@gmail.com>
2025-02-14 20:34:12 -0500

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    for (const value of values) {
+        let index = arr.indexOf(value);
+        if (index === -1) {
+            continue;
+        }
+        while (index !== -1) {
+            arr.splice(index, 1);
+            index = arr.indexOf(value, index);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
