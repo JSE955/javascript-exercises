ae83bcc2e515c733587ce98cde8086ec76321f28
JSE955 <jordanscottedwards@gmail.com>
2025-02-19 11:49:51 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return 'OOPS';
+    }
+    if (typeof num !== 'number') {
+        num = Number(num);
+    }
+    if (num === 0) {
+        return 0;
+    }
+    if (num === 1 || num === 2) {
+        return 1;
+    }
+    let seq = [1, 1];
+    for (let i = 3; i <= num; i++) {
+        seq.push(seq[i - 3] + seq[i - 2]);
+    }
+    return seq[num - 1];
 
 };
 
