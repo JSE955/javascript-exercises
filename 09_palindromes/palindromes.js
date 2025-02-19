c9d951ece8f24d9f18ef0489ec20a3b204cb30a1
JSE955 <jordanscottedwards@gmail.com>
2025-02-19 11:21:53 -0500

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let acceptable = 'abcdefghijklmnopqrstuvwxyz1234567890';
 
+    let cleanedArray = input.toLowerCase()
+                             .split('')
+                             .filter((char) => acceptable.includes(char));
+    let cleanedString = cleanedArray.join('');
+    let cleanedStringReversed = cleanedArray.reverse().join('');
+    return cleanedString === cleanedStringReversed;
+                             
 };
 
 // Do not edit below this line
