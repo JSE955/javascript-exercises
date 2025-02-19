e52b10a55c6dd2123ed3b06a676c1d019d09937c
JSE955 <jordanscottedwards@gmail.com>
2025-02-19 12:21:12 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, nextPerson) => {
+        if (oldest.yearOfDeath === undefined) {
+            oldest.yearOfDeath = Number(new Date().getFullYear());
+        }
+        if (nextPerson.yearOfDeath === undefined) {
+            nextPerson.yearOfDeath = Number(new Date().getFullYear());
+        }
+        let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        let nextPersonAge = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+        if (nextPersonAge > oldestAge) {
+            return nextPerson;
+        } else {
+            return oldest;
+        }
+    });
 };
 
 // Do not edit below this line
